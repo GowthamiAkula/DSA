771846c6f772a72d17394cce46a14a1600f09fac
GowthamiAkula <147791859+GowthamiAkula@users.noreply.github.com>
2025-02-24 11:12:16 +0530

    Time: 3311 ms (5.21%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -5,3 +5,14 @@ ON e.departmentId = d.id
 WHERE(departmentId,Salary) IN
 (SELECT departmentId,MAX(salary) FROM Employee
 GROUP BY departmentId)
+
+/* another approach
+WITH cte AS
+(
+    SELECT d.name Department,e.name Employee , e.salary,
+    MAX(e.salary) OVER(PARTITION BY d.id) AS mx_salary
+    FROM
+    employee e JOIN department d
+    ON e.departmentId = d.id;
+)
+SELECT department,employee,salary FROM cte WHERE salary = mx_salary; */
