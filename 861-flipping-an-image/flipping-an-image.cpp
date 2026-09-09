85e65dc4af3022da2fa056a52f9b12d656be0442
GowthamiAkula <147791859+GowthamiAkula@users.noreply.github.com>
2026-09-09 14:33:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (93.35%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.cpp
+++ 861-flipping-an-image/flipping-an-image.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
-        for(int i=0;i<image.size();i++)
+
+        for(int i = 0; i < image.size(); i++)
         {
-            reverse(image[i].begin(),image[i].end());
-            for(int j=0;j<image[i].size();j++)
+            int left = 0;
+            int right = image[i].size() - 1;
+
+            while(left <= right)
             {
-                if(image[i][j]==0)
-                image[i][j]=1;
-                else
-                image[i][j]=0;
+                int temp = image[i][left];
+
+                image[i][left] = 1 - image[i][right];
+                image[i][right] = 1 - temp;
+
+                left++;
+                right--;
             }
         }
+
         return image;
     }
 };
